b394e33b6f9b015d3005d1aef133cd2a7836a854
wi113512 <86160@student.pb.edu.pl>
2025-05-27 03:10:30 +0200

    działa zwykła notatka

--- Notatnik/Views/NoteDetailsWindow.xaml.cs
+++ Notatnik/Views/NoteDetailsWindow.xaml.cs
@@ -1,19 +1,67 @@
-﻿using System.Windows;
+﻿using System;
+using System.IO;
+using System.Text;
+using System.Windows;
+using System.Windows.Documents;
+using System.Windows.Markup;
+using Notatnik.Models;
 using Notatnik.ViewModels;
 
 namespace Notatnik.Views
 {
     public partial class NoteDetailsWindow : Window
     {
+        private readonly NoteDetailsViewModel _vm;
+
         public NoteDetailsWindow(NoteDetailsViewModel vm)
         {
             InitializeComponent();
+
+            _vm = vm;
             DataContext = vm;
-            vm.RequestClose += (s, result) =>
+
+            // Obsługa zapisu sformatowanej treści przy zamknięciu
+            _vm.RequestClose += OnVmRequestClose;
+            Loaded += OnLoaded;
+        }
+
+        private void OnLoaded(object sender, RoutedEventArgs e)
+        {
+            // Jeśli edytujemy LongFormat i mamy już jakąś zawartość – załaduj ją
+            if (_vm.Note.Type == NoteType.LongFormat
+                && !string.IsNullOrEmpty(_vm.Note.Content))
             {
-                DialogResult = result;
-                Close();
-            };
+                var range = new TextRange(RichTextEditor.Document.ContentStart, RichTextEditor.Document.ContentEnd);
+                try
+                {
+                    using var ms = new MemoryStream(Encoding.UTF8.GetBytes(_vm.Note.Content));
+                    range.Load(ms, DataFormats.Xaml);
+                }
+                catch
+                {
+                    // nie udało się załadować XAML-a – zostaw pusty dokument
+                }
+            }
+        }
+
+        private void OnVmRequestClose(object sender, bool dialogResult)
+        {
+            if (dialogResult)
+            {
+                // Przed zapisaniem DTO do bazy: wyciągnij XAML z RichTextBoxa
+                if (_vm.Note.Type == NoteType.LongFormat)
+                {
+                    var range = new TextRange(RichTextEditor.Document.ContentStart, RichTextEditor.Document.ContentEnd);
+                    using var ms = new MemoryStream();
+                    range.Save(ms, DataFormats.Xaml);
+                    _vm.Note.Content = Encoding.UTF8.GetString(ms.ToArray());
+                }
+                // RegularPanel i ChecklistPanel już mają dwukierunkowe bindingi na Content i ChecklistItems
+            }
+
+            // Zamknij okno jako dialog
+            DialogResult = dialogResult;
+            Close();
         }
     }
 }
